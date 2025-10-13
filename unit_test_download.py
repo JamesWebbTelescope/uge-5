7adcdb36eae79571409975cda66ce65079be869d
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-13 11:16:21 +0200

    Update unit_test_download.py

--- unit_test_download.py
+++ unit_test_download.py
@@ -1,1 +1,11 @@
 import unittest
+from unittest.mock import patch
+from download_files import PDFDownloader
+
+class test_url_methods(unittest.TestCase):
+
+    def setUp(self):
+        def setUp():
+            downloader = PDFDownloader()
+
+        
